ea8f16915fd3b5b140c6863f09a6e7dc7b5f6b8a
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-01-24 20:05:53 +0100

    application

---
+++ lib/copy_backups_to_drive/application.rb
@@ -0,0 +1,20 @@
+require 'copy_backups_to_drive/reactor'
+
+module CopyBackupsToDrive
+  class Application
+    def initialize
+      @reactor = Reactor.new
+    end
+
+    def run
+      @reactor.pending_work_handler { puts "work handler" }
+
+      trap("INT") {
+        @reactor.schedule { puts "scheduled" }
+        @reactor.interrupt_sleep
+      }
+
+      @reactor.run
+    end
+  end
+end
