a165ce7b5d67070d0a8605f899840add8632d3dd
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-01-26 16:42:21 +0100

    more methods on Chain

--- lib/triplicity/site/chain.rb
+++ lib/triplicity/site/chain.rb
@@ -1,12 +1,30 @@
 module Triplicity
   module Site
     class Chain
+      attr_reader :assets
+
       def initialize(assets)
         @assets = assets
       end
 
       def inspect
         "##{self.class.name}: #{@assets.first.manifest_path}>"
+      end
+
+      def size
+        @assets.map(&:size).reduce(:+)
+      end
+
+      def pessimistic_size
+        @assets.map(&:pessimistic_size).reduce(:+)
+      end
+
+      def remove
+        @assets.reverse.each(&:remove)
+      end
+
+      def copy_to(target)
+        @assets.each { |asset| asset.copy_to target }
       end
     end
   end
