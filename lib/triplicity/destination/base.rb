3399881cba46f6d9cc9bc5288f8eb0636d9a9290
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-01-26 19:42:39 +0100

    move max_space and youngest_timestamp to Destination::Base

--- lib/triplicity/destination/base.rb
+++ lib/triplicity/destination/base.rb
@@ -1,7 +1,33 @@
+require 'digest'
+require 'json'
+
 module Triplicity
   module Destination
     class Base
-      attr_reader :max_size
+      attr_reader :max_space
+      attr_accessor :youngest_timestamp
+
+      def initialize(config = {})
+        @max_space = canonicalize_max_space(config['max_space'])
+      end
+
+      def canonicalize_max_space(value)
+        return Float(value) if value.is_a?(Numeric)
+
+        match = /^([\d.]+)([kKmMgGtT]?)[bB]?$/.match(value)
+
+        raise 'bad format' unless match
+        num = Float(match[1])
+        exp_factor = {
+          ''  => 1,
+          'k' => 1024,
+          'm' => 1024**2,
+          'g' => 1024**3,
+          't' => 1024**4,
+        }.fetch(match[2].downcase)
+
+        num * exp_factor
+      end
     end
   end
 end
--- lib/triplicity/destination/fs_path_on_device.rb
+++ lib/triplicity/destination/fs_path_on_device.rb
@@ -18,24 +18,6 @@ module Triplicity
 
       private
 
-      def canonicalize_max_space(value)
-        return Float(value) if value.is_a?(Numeric)
-
-        match = /^([\d.]+)([kKmMgGtT]?)[bB]?$/.match(value)
-
-        raise 'bad format' unless match
-        num = Float(match[1])
-        exp_factor = {
-          ''  => 1,
-          'k' => 1024,
-          'm' => 1024**2,
-          'g' => 1024**3,
-          't' => 1024**4,
-        }.fetch(match[2].downcase)
-
-        num * exp_factor
-      end
-
       def current_path
         fs = mounted_filesystems.find { |s| s[:uuid] == device_uuid }
         Pathname(fs[:mountpoint]) + rel_path if fs
