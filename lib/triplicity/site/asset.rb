f419f8d4ed16cf8b55c543415efff14c804b4a4a
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-01-26 16:14:13 +0100

    extract asset into proper file

---
+++ lib/triplicity/site/asset.rb
@@ -0,0 +1,78 @@
+require 'pathname'
+require 'fileutils'
+
+module Triplicity
+  class Site
+    class Asset
+      attr_accessor :base, :basing
+
+      def initialize(manifest_path)
+        @manifest_path = manifest_path
+        @full_basename = (manifest_path.parent + manifest_path.basename('.manifest')).to_s
+      end
+
+      def timestamp
+        full? ? match_segments[2] : timestamp_to
+      end
+
+      def timestamp_from
+        match_segments[2]
+      end
+
+      def timestamp_to
+        match_segments[3]
+      end
+
+      def incremental?
+        match_segments[1] == 'inc'
+      end
+
+      def full?
+        !incremental?
+      end
+
+      def paths
+        @paths ||= calculate_paths
+      end
+
+      def size
+        paths.map(&:size).reduce(:+)
+      end
+
+      def pessimistic_size
+        size + paths.length * 4096
+      end
+
+      def inspect
+        "##{self.class.name}: #{@manifest_path}>"
+      end
+
+      def remove
+        paths.reverse.each do |path|
+          path.unlink
+        end
+      end
+
+      def copy_to(target)
+        from_paths = [*paths.drop(1), paths.first]
+        FileUtils.cp(from_paths, target)
+      end
+
+      private
+
+      TIMESTAMP_RE = '[\dTZ]{16}'
+      CAPTURE_TS = "(#{TIMESTAMP_RE})"
+
+      def match_segments
+        @match_segments ||= %r{-(inc|full)\.#{CAPTURE_TS}(?:\.to\.#{CAPTURE_TS})?}.match(@full_basename)
+      end
+
+      def calculate_paths
+        [
+          @manifest_path,
+          *Pathname.glob(@full_basename + '*.difftar.gz'),
+        ]
+      end
+    end
+  end
+end
