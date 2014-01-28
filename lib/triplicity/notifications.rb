4fb11e8a4e84de6df79309b1d64e58e471de27fb
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-01-28 21:30:30 +0100

    Notifications

---
+++ lib/triplicity/notifications.rb
@@ -0,0 +1,67 @@
+require 'dbus'
+
+module Triplicity
+  class Notifications
+    attr_accessor :app_name
+
+    class Notification
+      attr_accessor :app_name, :icon, :summary, :body, :buttons, :hints, :timeout
+      attr_reader :id
+
+      def initialize(interface)
+        @interface = interface
+        default_expire!
+        if block_given?
+          yield self
+          issue
+        end
+      end
+
+      def default_expire!
+        self.timeout = -1
+      end
+
+      def never_expire!
+        self.timeout = 0
+      end
+
+      def issue
+        yield self if block_given?
+
+        app_name = self.app_name || ''
+        icon = self.icon || ''
+        summary = self.summary || ''
+        body = self.body || ''
+        buttons = self.buttons || []
+        hints = self.hints || {}
+        replaces_id = @id || 0
+
+        result = @interface.Notify(app_name, replaces_id, icon, summary, body, buttons, hints, timeout)
+        @id = result.first
+
+        self
+      end
+    end
+
+    def issue
+      Notification.new(interface) do |notification|
+        notification.app_name = @app_name if @app_name
+        yield notification
+      end
+    end
+
+    private
+
+    def interface
+      object['org.freedesktop.Notifications']
+    end
+
+    def object
+      service.object('/org/freedesktop/Notifications').tap(&:introspect)
+    end
+
+    def service
+      DBus.session_bus["org.freedesktop.Notifications"]
+    end
+  end
+end
