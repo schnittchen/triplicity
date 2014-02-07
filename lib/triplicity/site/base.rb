ebce74054cb40627a967fe6c8c4b63e36f832abc
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-02-07 23:43:47 +0100

    Site: extract general logic into a new base class

---
+++ lib/triplicity/site/base.rb
@@ -0,0 +1,50 @@
+require 'triplicity/sorting'
+require 'triplicity/site/asset'
+require 'triplicity/site/chain'
+
+module Triplicity
+  module Site
+    class Base
+      def chains
+        @chains ||= calculate_chains
+      end
+
+      def latest_timestamp
+        chains.map(&:latest_timestamp).max
+      end
+
+      def operations
+        raise NotImplementedError
+      end
+
+      private
+
+      include Sorting
+
+      def calculate_chains
+        oldest_first(asset_candidates).slice_before(&:full?).map do |assets|
+          next nil unless assets.first.full?
+
+          assets.each_cons(2) do |pred, succ|
+            next unless succ.timestamp_from == pred.timestamp
+            succ.base = pred
+            pred.basing = succ
+          end
+
+          assets = [
+            assets.first,
+            *assets.drop(1).take_while(&:base)
+          ]
+
+          Chain.new(assets)
+        end.compact
+      end
+
+      def asset_candidates
+        operations.glob('*.manifest').map do |name|
+          Asset.new(self, name)
+        end
+      end
+    end
+  end
+end
--- lib/triplicity/site/instance.rb
+++ lib/triplicity/site/instance.rb
@@ -1,24 +1,14 @@
-require 'pathname'
+require 'triplicity/site/base'
 
-require 'triplicity/sorting'
-require 'triplicity/site/asset'
-require 'triplicity/site/chain'
+require 'pathname'
 
 module Triplicity
   module Site
-    class Instance
+    class Instance < Base
       attr_accessor :path
 
       def initialize(path)
         @path = Pathname(path)
-      end
-
-      def chains
-        @chains ||= calculate_chains
-      end
-
-      def latest_timestamp
-        chains.map(&:latest_timestamp).max
       end
 
       class Operations
@@ -52,35 +42,6 @@ module Triplicity
 
       def operations
         @operations ||= Operations.new(@path)
-      end
-
-      private
-
-      include Sorting
-
-      def calculate_chains
-        oldest_first(asset_candidates).slice_before(&:full?).map do |assets|
-          next nil unless assets.first.full?
-
-          assets.each_cons(2) do |pred, succ|
-            next unless succ.timestamp_from == pred.timestamp
-            succ.base = pred
-            pred.basing = succ
-          end
-
-          assets = [
-            assets.first,
-            *assets.drop(1).take_while(&:base)
-          ]
-
-          Chain.new(assets)
-        end.compact
-      end
-
-      def asset_candidates
-        operations.glob('*.manifest').map do |name|
-          Asset.new(self, name)
-        end
       end
     end
   end
