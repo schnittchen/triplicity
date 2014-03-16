2c291b681a9048f712b000c12c3005a83afd33d9
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-03-16 20:55:40 +0100

    move UpToDateness to its own file

--- lib/triplicity/destination/base.rb
+++ lib/triplicity/destination/base.rb
@@ -13,6 +13,7 @@ require 'triplicity/util/on_when'
 require 'triplicity/util/has_cache'
 require 'triplicity/sync_action'
 require 'triplicity/looping_thread'
+require 'triplicity/destination/up_to_dateness'
 
 module Triplicity
   module Destination
@@ -46,48 +47,6 @@ module Triplicity
       on_when.event :successful_operation
       on_when.event :unsuccessful_operation
       on_when.event :ended_connection
-
-      class UpToDateness
-        include Util::OnWhen
-
-        on_when.event :lost
-        on_when.delegates_subscriptions(self)
-
-        alias_method :when_lost, :on_lost # @TODO
-
-        def initialize(cache, cache_ident)
-          @cache, @cache_ident = cache, cache_ident
-          @destination_timestamp = @cache.destination_latest_timestamp(@cache_ident)
-          @on_when = on_when_new
-        end
-
-        def given?
-          # always assume there is nothing to do until at least we know about the state of the primary
-          !@primary_timestamp || (
-            # unknown destination timestamp implies not up to date
-            @primary_timestamp == @destination_timestamp
-          )
-        end
-
-        ## this might be interesting for notification messages
-        # def destination_timestamp_known?
-        #   !!@destination_timestamp
-        # end
-
-        def primary_timestamp_changed(timestamp)
-          @primary_timestamp = timestamp
-          on_when.trigger_lost unless given?
-        end
-
-        def update_destination_timestamp(timestamp)
-          # it is assumed that this is not called in a re-entrant fashion
-          # (because it is called at the end of a sync operation which
-          # ensures it is only happening once at a time)
-          @cache.destination_latest_timestamp(@cache_ident, timestamp)
-          @destination_timestamp = timestamp
-          on_when.trigger_lost unless given?
-        end
-      end
 
       def initialize(options, primary, application)
         @primary = primary
---
+++ lib/triplicity/destination/up_to_dateness.rb
@@ -0,0 +1,58 @@
+#
+# up_to_dateness.rb - manages whether a destination is up to date
+#
+# Copyright (C) 2014 Thomas Stratmann <thomas.stratmann@rub.de>
+# All rights reserved.
+# This is free software with ABSOLUTELY NO WARRANTY.
+#
+# You can redistribute it and/or modify it under the terms of
+# the GNU General Public License version 2.
+#
+
+require 'triplicity/util/on_when'
+
+module Triplicity
+  module Destination
+    class UpToDateness
+      include Util::OnWhen
+
+      on_when.event :lost
+      on_when.delegates_subscriptions(self)
+
+      alias_method :when_lost, :on_lost # @TODO
+
+      def initialize(cache, cache_ident)
+        @cache, @cache_ident = cache, cache_ident
+        @destination_timestamp = @cache.destination_latest_timestamp(@cache_ident)
+        @on_when = on_when_new
+      end
+
+      def given?
+        # always assume there is nothing to do until at least we know about the state of the primary
+        !@primary_timestamp || (
+          # unknown destination timestamp implies not up to date
+          @primary_timestamp == @destination_timestamp
+        )
+      end
+
+      ## this might be interesting for notification messages
+      # def destination_timestamp_known?
+      #   !!@destination_timestamp
+      # end
+
+      def primary_timestamp_changed(timestamp)
+        @primary_timestamp = timestamp
+        on_when.trigger_lost unless given?
+      end
+
+      def update_destination_timestamp(timestamp)
+        # it is assumed that this is not called in a re-entrant fashion
+        # (because it is called at the end of a sync operation which
+        # ensures it is only happening once at a time)
+        @cache.destination_latest_timestamp(@cache_ident, timestamp)
+        @destination_timestamp = timestamp
+        on_when.trigger_lost unless given?
+      end
+    end
+  end
+end
